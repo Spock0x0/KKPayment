dc65b344e8d2e42a28f80f2c5a40da91b302fab5
Spock <spock.hsueh@kkday.com>
2022-08-30 10:12:37 +0800

    first commit

---
+++ Example/Pods/Target Support Files/KKPayment/KKPayment-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_KKPayment : NSObject
+@end
+@implementation PodsDummy_KKPayment
+@end
---
+++ Test.swift
@@ -0,0 +1,8 @@
+//
+//  test.swift
+//  KKPayment
+//
+//  Created by Spock on 2022/8/30.
+//
+
+import PromiseKit
